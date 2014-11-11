51171bb5acd5beaf52120ccc3721172116faf5a7
Andy Duncan <andy@duncaningram.com>
2014-11-11 02:09:38 -0500

    Added some bootstrap, less, user crud.

--- rmq.js
+++ rmq.js
@@ -1,18 +1,14 @@
 if (Meteor.isClient) {
-  // counter starts at 0
   Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
+  Template.addrequest.helpers({
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
+  Template.addrequest.events({
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
 
