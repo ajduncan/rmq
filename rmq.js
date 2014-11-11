2a209d3e23fbfcfb06039a96383b5b146104a9fd
Andy Duncan <ajduncan@gmail.com>
2014-11-11 16:11:48 -0500

    Added some iron routing and created a /requests list flow.

--- rmq.js
+++ rmq.js
@@ -1,5 +1,40 @@
+Requests = new Mongo.Collection("requests");
+// db.requests.insert({ text: "Hello world!", createdAt: new Date(), owner: "LOLSMEE" });
+
+Router.route('/', function() {
+  this.render('home');
+});
+
+Router.map(function() {
+
+  this.route('requests', {
+    data: function() {
+      templateData = { requests: Requests.find({}) };
+      return templateData;
+    }
+  });
+
+});
+
 if (Meteor.isClient) {
   Session.setDefault("counter", 0);
+
+  Template.body.events({
+    "submit .new-request": function (event) {
+      // This function is called when the new task form is submitted
+
+      var text = event.target.text.value;
+      var name = event.target.name.value;
+      Meteor.call("addRequest", text, name);
+
+      // Clear form
+      event.target.text.value = "";
+      event.target.name.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  });
 
   Template.addrequest.helpers({
   });
@@ -11,6 +46,30 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 }
+
+Meteor.methods({
+  addRequest: function (text) {
+    Requests.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: name /*, Meteor.userId(), */
+      /* username: Meteor.user().username */
+    });
+  },
+  deleteRequest: function (requestId) {
+    var request = Requests.findOne(requestId);
+    // Need to make sure Meteor user is an admin.
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Requests.remove(requestId);
+  },
+  updateRequest: function (requestId, text) {
+    Requests.update(requestId, { $set: { text: text } });
+  },
+});
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
