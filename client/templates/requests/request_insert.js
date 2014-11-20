7fff51f7240f3ac78f767d90e56f6d299a6dcb3f
Andy Duncan <ajduncan@gmail.com>
2014-11-20 17:34:11 -0500

    Handle updates on success the AutoForm way with hooks, don't bother with custom overrides for now.  Also, use the resetOnSuccess attribute of the template form.

--- client/templates/requests/request_insert.js
+++ client/templates/requests/request_insert.js
@@ -1,37 +1,3 @@
-Template.insertRequest.events({
-  "submit #insertRequestForm": function (event) {
-    // This function is called when the new request form is submitted
-
-    var first_name = event.target.first_name.value;
-    var last_name = event.target.last_name.value;
-    var email = event.target.email.value;
-    var subject = event.target.subject.value;
-    var category = event.target.category.value;
-    var description = event.target.description.value;
-
-    Meteor.call("insertRequest", first_name, last_name, email, subject, category, description, 
-        function (error, result) {
-            if (error) {
-                alert(error);
-            } else {
-                bootbox.alert('Request added!');
-            }
-        }
-    );
-
-    // Clear form
-    event.target.first_name.value = "";
-    event.target.last_name.value = "";
-    event.target.email.value = "";
-    event.target.subject.value = "";
-    event.target.description.value = "";
-    // how to clear category to default?
-
-    // Prevent default form submit
-    return false;
-  }
-});
-
 Template.insertRequest.helpers({
     categoryOptions: function() {
         var options = [];
@@ -41,5 +7,11 @@ Template.insertRequest.helpers({
             })
         });
         return options;
+    }
+});
+
+AutoForm.addHooks('insertRequestForm', {
+    onSuccess: function () {
+        bootbox.alert('Request added!');
     }
 });
